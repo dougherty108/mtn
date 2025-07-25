053a1807028ee690e40337427d1e82137955f761
Charlie Dougherty <78181516+dougherty108@users.noreply.github.com>
2025-07-25 15:55:40 -0600

    Encountering a weird error when trying to load the YSI data (.csv format). not quite sure what that's about, it says something about a string error.

--- R/TUC_munging_20250724.R
+++ R/TUC_munging_20250724.R
@@ -3,39 +3,82 @@
 # load packages
 library(tidyverse)
 
-# load in miniDOT files (and wiper data if that's available)
-# since it's miniDOT files, it'll be a list of .txt files that need to be concatonated
-top_DO = read_delim("../../../../OneDrive-SharedLibraries-UCB-O365/Mountain limnology lab - Data/Sensors/miniDOT/TUC_0.5_BOT/2024-07-17 142700Z.txt",
-                    delim = ",", skip = 2)
-
-
-# point R to the directory where the bottom logger files are contained
-miniDOT_bot_dir = list.files ("../../../../OneDrive-SharedLibraries-UCB-O365/Mountain limnology lab - Data/Sensors/miniDOT/TUC_0.5_BOT/")
-
-combined_df = data.frame()
-
 setwd("/Users/chdo4929/Library/CloudStorage/OneDrive-SharedLibraries-UCB-O365/Mountain limnology lab - Data/Sensors/miniDOT/TUC_0.5_BOT/")
-# for loop
+# point R to the directory where the bottom logger files are contained
+miniDOT_bot_dir = list.files()
+
+combined_bot = data.frame()
+# bottom sensor
 for (file_name in miniDOT_bot_dir) {
   # Read the current file into a temporary data frame
-  temp_df <- read_delim(file_name, delim = ",", skip = 2) # You can use other functions like read.delim for different file types
-  
+  temp_df <- read_delim(file_name, delim = ",", skip = 2) 
   # Append the temporary data frame to the combined data frame
-  combined_df <- rbind(combined_df, temp_df)
+  combined_bot <- rbind(combined_bot, temp_df)
 }
 
-
-combined_corrected = combined_df %>% 
+combined_bottom = combined_bot %>% 
   mutate(`  DO (mg/l)` = as.numeric(`  DO (mg/l)`), 
          `  T (deg C)` = as.numeric(`  T (deg C)`), 
-         date = as.POSIXct(origin = "1970-01-01)) %>% 
-  filter(`  DO (mg/l)` < 1.00)
+         date = as.POSIXct(origin = "1970-01-01", x = `Time (sec)`))
+
+# set wd to the top sensor
+setwd("/Users/chdo4929/Library/CloudStorage/OneDrive-SharedLibraries-UCB-O365/Mountain limnology lab - Data/Sensors/miniDOT/TUC_13.5_BOT/")
+# point R to the directory where the bottom logger files are contained
+miniDOT_top_dir = list.files()
+
+combined_surf = data.frame()
+# bottom sensor
+for (file_name in miniDOT_top_dir) {
+  # Read the current file into a temporary data frame
+  temp_df <- read_delim(file_name, delim = ",", skip = 2) 
+  
+  # Append the temporary data frame to the combined data frame
+  combined_surf <- rbind(combined_surf, temp_df)
+}
+
+combined_surface = combined_surf %>% 
+  mutate(`  DO (mg/l)` = as.numeric(`  DO (mg/l)`), 
+         `  T (deg C)` = as.numeric(`  T (deg C)`), 
+         date = as.POSIXct(origin = "1970-01-01", x = `Time (sec)`))
+
+combined_bottom = combined_bottom %>% 
+  mutate(`depth from bottom` = "0.5") %>% 
+  filter(`  DO (mg/l)` < 0.20)
+
+combined_surface = combined_surface %>% 
+  mutate(`depth from bottom` = "13.5")
+
+# how complete is the data between the two sensors? 
+print(nrow(combined_bottom))
+print(nrow(combined_surface))
+
+#print difference between data
+print(nrow(combined_bottom) - nrow(combined_surface))
+
+combined_df = rbind(combined_bottom, combined_surface)
+
+TUC_DO = combined_df %>% 
+  filter(date > "2024-07-18" & date < "2025-07-15") %>% 
+  mutate(month = as.character(month(date)))
 
 
 
 # plot
-ggplot(combined_corrected, aes(`Time (sec)`, `  DO (mg/l)`)) + 
-         geom_path()
+ggplot(TUC_DO, aes(date, `  DO (mg/l)`)) + 
+         geom_point(shape = 21, size = 2) + 
+  geom_jitter() +
+  facet_wrap(vars(`depth from bottom`), scales = "free") + 
+  theme_bw() + 
+  ggtitle("DO (mg/L), Turkey Creek Lake 2024-2025")
+
+# plot by month
+ggplot(TUC_DO, aes(date, `  DO (mg/l)`, color = month)) + 
+  geom_point(shape = 21, size = 2) + 
+  geom_jitter() +
+  facet_wrap(vars(`depth from bottom`), scales = "free") + 
+  theme_bw() + 
+  ggtitle("DO (mg/L), Turkey Creek Lake 2024-2025 by month")
+
 
 
 
