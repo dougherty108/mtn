ca79c0382abc8fdedf8d9e50d3f2a89853932c13
Charlie Dougherty <78181516+dougherty108@users.noreply.github.com>
2025-08-08 11:14:59 -0600

    Stashing this away before running off to the lab for abit.

--- R/Sterling_Ranch_GIS_munging.R
+++ R/Sterling_Ranch_GIS_munging.R
@@ -41,22 +41,21 @@ pond_webmerc <- st_transform(pond_sf, 3857)
 open_channel_webmerc <- st_transform(open_channel_sf, 3857)
 
 # Get the basemap (you can change provider, e.g., "CartoDB.Positron", "OpenStreetMap", etc.)
-basemap <- get_tiles(conduit_webmerc, provider = "CartoDB.Positron", crop = TRUE)
+
+#Conduit
+basemap <- get_tiles(junction_webmerc, provider = "CartoDB.Positron", crop = TRUE)
 
 ggplot() +
   layer_spatial(basemap) +  # Basemap layer
-  geom_sf(data = open_channel_webmerc, 
-          #aes(#fill = OPEN_CHANNEL_TYPE), 
-          size = 0.5) +  # Your data
-  geom_sf(data = pond_webmerc, 
-          aes(fill = COLOR_RANK), 
-          size = 0.5) +  
-  geom_sf(data = inlet_webmerc, 
-          aes(color = ), 
-          size = 0.5) + 
+  geom_sf(data = junction_webmerc, 
+          aes(color = as.character(JUNCTION_SIZE)), 
+          size = 1.0) +  # Your data
+  geom_sf(
+    data = open_channel_sf
+  ) +
   annotation_scale(location = "bl") +
   annotation_north_arrow(location = "tl", which_north = "true") +
-  theme_minimal() +
-  labs(title = "Sterling Ranch", color = "Legend Title")
+  labs(title = "Sterling Ranch--Junctions", color = "Junction Size") +
+  theme_minimal()
 
-plot(inlet_webmerc["Shape"])
+
